9940e12ba978026fb75fec7964f903c2154bd40a
roks0n <haluzan.rok@gmail.com>
2018-07-07 13:53:25 +0200

    small changes to hex.writer methods

--- binary/hex/writer.py
+++ binary/hex/writer.py
@@ -2,12 +2,11 @@ from binascii import unhexlify
 from struct import pack
 
 
-def write_low(data, nibble=None):
+def write_low(data):
     """Write a hex string with low nibble first
 
     Args:
         data (int)
-        nibble (str, optional): format string
 
     Returns:
         bytes: bytes object containing data
@@ -15,12 +14,11 @@ def write_low(data, nibble=None):
     return pack('<B', data)
 
 
-def write_high(data, nibble=None):
+def write_high(data):
     """Write a hex string with high nibble first
 
     Args:
         data (int)
-        nibble (str, optional): format string
 
     Returns:
         bytes: bytes object containing data
